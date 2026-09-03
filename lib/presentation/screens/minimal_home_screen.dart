283f49d2c91cc6c86a266ae295470963ec40ae5d
kendrewbuglione-dot <kendrewbuglione@gmail.com>
2026-09-04 04:01:45 +0700

    Add missing vpn_controller import to minimal_home_screen

--- lib/presentation/screens/minimal_home_screen.dart
+++ lib/presentation/screens/minimal_home_screen.dart
@@ -1,3 +1,4 @@
+import "../controllers/vpn_controller.dart";
 import 'package:flutter/material.dart';
 import '../controllers/vpn_controller.dart';
 import '../../core/state/failover_state_machine.dart';
